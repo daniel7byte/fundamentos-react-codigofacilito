151bd4a3630d6b404934d654871f80ebce7b3bb3
daniel7byte <daniel7byte@gmail.com>
2020-06-14 11:50:08 -0500

    props

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-function MiComponente(){
-  return <p>Hola Mundo</p>;
+function MiComponentePresentacion(props){
+  return <p>Hola soy {props.nombre} y tengo {props.edad} años de edad</p>;
 }
 
 class MiComponenteDeClase extends Component {
@@ -22,13 +22,14 @@ class App extends Component {
   }
 
   render() {
+    let edad = 18;
     return (
       <div>
         <Hello name={this.state.name} />
         <p>
           Start editing to see some magic happen :)
         </p>
-        <MiComponente/>
+        <MiComponentePresentacion nombre="Jose Daniel" edad={18}/>
         <MiComponenteDeClase/>
       </div>
     );
